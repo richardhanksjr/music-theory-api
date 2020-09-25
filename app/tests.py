e4ae6b2ec17f4fe23bc82133de3a33d93d5158c6
MichaelKramerGuitar <michael.kramer.guitar@gmail.com>
2020-09-24 22:04:11 -0400

    added question (and responses) to questions.html and passing test it exists

--- app/tests.py
+++ app/tests.py
@@ -12,4 +12,12 @@ class QuestionsTest(TestCase):
         self.client = Client()
         response = self.client.get("/questions/")
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'app/questions.html')
+
+    def test_question_exists_on_questions_page(self):
+        self.client = Client()
+        response = self.client.get("/questions/")
+        # we'll have to adjust this to be dynamic once the questions are being dynamically generated.
+        self.assertContains(response, "What is the")
+
 
