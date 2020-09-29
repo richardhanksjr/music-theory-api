fda457699d894aa3e5c5264a0eb216c0a7b821f1
nkawaller <63615939+nkawaller@users.noreply.github.com>
2020-09-29 09:37:49 -0400

    test logging in via email

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,7 +1,9 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 from django.urls import reverse, resolve
 
-# Create your tests here.
+from django.contrib.auth.models import User
+
+Create your tests here.
 
 class SignupTests(TestCase):
 
@@ -18,3 +20,35 @@ class SignupTests(TestCase):
         self.assertContains(self.response, 'Sign Up')
         self.assertNotContains(
             self.response, 'Hi there! I should not be on the page.')
+
+class LoginTests(TestCase):
+
+    """ 
+    As a user I would link a user model where email is the username so that I can 
+    log into the app using my email
+    """
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+                                username='newuser', 
+                                email='newuser@email.com',
+                                password='testpass123')
+        user_two = User.objects.create_user(
+                                username='usertwo', 
+                                email='usertwo@email.com',
+                                password='testpass222')
+        user_three = User.objects.create_user(
+                                username='userthree', 
+                                email='userthree@email.com',
+                                password='testpass333')
+
+    def test_login_returns_status_code_200(self):
+        url = reverse('account_login')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_using_email_as_id(self):
+        c = Client()
+        login = c.login(email='newuser@email.com', password='testpass123')
+        self.assertEqual(login, True)
--- music_theory_api/settings.py
+++ music_theory_api/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
     'allauth',
     'allauth.account',
+    'allauth.socialaccount',
     'accounts.apps.AccountsConfig',
     'app',
     'api'
