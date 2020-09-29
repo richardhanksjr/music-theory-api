6fc8cc0203e8866d84d96c698dd718a26fc5f942
Natalie Sterling <nataliemetasterling@gmail.com>
2020-09-29 09:05:34 -0400

    working on loginrequiredmixin

--- app/views.py
+++ app/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.views.generic import View
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+# for all views, use LoginRequiredMixin
 
 
 class LandingPageView(View):
+
     def get(self, request):
         return render(request, 'app/landing.html')
