3825e7ab413d0ecb2efee2c0eb9139895e316c66
MichaelKramerGuitar <michael.kramer.guitar@gmail.com>
2020-10-01 08:46:54 -0400

    deleted lines in app/views.py for test route and reference in app/urls.pu to that route

--- app/views.py
+++ app/views.py
@@ -1,9 +1,2 @@
 
-from django.http import HttpResponse
-from django.views import View
-
-
-class TestRoute(View):
-    def get(self, request):
-        return HttpResponse("Successfully deployed from GitHub branch. Hey! This is Nathan checking in.")
 
