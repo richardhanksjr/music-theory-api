ffeebc65ff024467b1835e589b692684fc6311bb
Richard Hanks <richardhanksjr@gmail.com>
2020-10-02 12:36:56 -0400

    Added answer route.

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,38 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 from django.test import TestCase
+from rest_framework.test import APIClient
+from questions.questions.simple_questions import SimpleIntervalIs
 
-# Create your tests here.
+User = get_user_model()
+
+
+class TestAnswer(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.simple_interval = SimpleIntervalIs()
+        cls.client = APIClient()
+        user = User.objects.create_user(username="foo")
+        cls.client.force_login(user)
+
+    def test_return_correct_answer(self):
+        key = self.simple_interval.key
+        answer = self.simple_interval.answer
+        url = reverse('answer')
+        response = self.client.post(url, {'key': key, 'answer': answer}).data
+        self.assertTrue(response['correct'])
+        self.assertEqual(response['correct_answer'], answer)
+
+    def test_return_incorrect_answer(self):
+        key = self.simple_interval.key
+        answer = "This is not the correct answer"
+        url = reverse('answer')
+        response = self.client.post(url, {'key': key, 'answer': answer}).data
+        self.assertFalse(response['correct'])
+        self.assertEqual(response['correct_answer'], self.simple_interval.answer)
+
+    def test_400_on_bad_request(self):
+        url = reverse('answer')
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, 400)
