0d073a9645aa0a31e9c5ca69442905704461ffc6
nkawaller <63615939+nkawaller@users.noreply.github.com>
2020-10-02 07:50:23 -0400

    trying some things out

--- accounts/tests.py
+++ accounts/tests.py
@@ -6,12 +6,26 @@ from django.contrib.auth.models import User
 
 class SignupTests(TestCase):
 
-    username = 'newuser'
-    email = 'newuser@email.com'
+    """ 
+    As a user I want a registration page so that I 
+    can register for an account with an email and password.
+    """
 
     def setUp(self):
         url = reverse('account_signup')
         self.response = self.client.get(url)
+        user_one = User.objects.create_user(
+                                username='newuser', 
+                                email='newuser@email.com',
+                                password='testpass123')
+        user_two = User.objects.create_user(
+                                username='usertwo', 
+                                email='usertwo@email.com',
+                                password='testpass222')
+        user_three = User.objects.create_user(
+                                username='userthree', 
+                                email='userthree@email.com',
+                                password='testpass333')
 
     def test_signup_template(self):
         self.assertEqual(self.response.status_code, 200)
@@ -19,6 +33,40 @@ class SignupTests(TestCase):
         self.assertContains(self.response, 'Sign Up')
         self.assertNotContains(
             self.response, 'Hi there! I should not be on the page.')
+
+    # def test_duplicate_email_does_not_create_two_users(self):
+    #     original_email = User.objects.get(email='newuser@email.com')
+    #     self.assertEqual(original_email.username, 'newuser')
+    #     duplicate_email = User.objects.create_user(
+    #                             username='duplicateuser', 
+    #                             email='newuser@email.com',
+    #                             password='duplicatepass')
+    #     user_count = User.objects.filter(email='newuser@email.com')
+    #     self.assertEqual(user_count.count(), 1)
+
+    def test_user_already_exists_redirected_to_login(self):
+        url = reverse('account_signup')
+        self.response = self.client.get(url)
+        self.assertEqual(self.response.status_code, 200)
+
+class VideoTests(TestCase):   
+    
+    def setUp(self):
+        self.client = Client()
+        self.signup_url = reverse('account_signup')
+        self.login_url = reverse('account_login')
+
+    def test_signup_GET(self):
+
+        response = self.client.get(self.signup_url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_GET(self):
+
+        response = self.client.get(self.login_url)
+        self.assertEqual(response.status_code, 200)
+
+
 
 class LoginTests(TestCase):
 
