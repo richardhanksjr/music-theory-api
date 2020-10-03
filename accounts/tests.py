25ebb4c7243d83ef091cab9b43b36f1ca77defde
nkawaller <63615939+nkawaller@users.noreply.github.com>
2020-10-03 11:12:29 -0400

    deleted some unused code

--- accounts/tests.py
+++ accounts/tests.py
@@ -34,49 +34,6 @@ class SignupTests(TestCase):
         self.assertNotContains(
             self.response, 'Hi there! I should not be on the page.')
 
-    # def test_duplicate_email_does_not_create_two_users(self):
-    #     original_email = User.objects.get(email='newuser@email.com')
-    #     self.assertEqual(original_email.username, 'newuser')
-    #     duplicate_email = User.objects.create_user(
-    #                             username='duplicateuser', 
-    #                             email='newuser@email.com',
-    #                             password='duplicatepass')
-    #     user_count = User.objects.filter(email='newuser@email.com')
-    #     self.assertEqual(user_count.count(), 1)
-
-    def test_user_already_exists_redirected_to_login(self):
-        url = reverse('account_signup')
-        self.response = self.client.get(url)
-        self.assertEqual(self.response.status_code, 200)
-
-class VideoTests(TestCase):   
-    
-    def setUp(self):
-        self.client = Client()
-        self.signup_url = reverse('account_signup')
-        self.login_url = reverse('account_login')
-
-    def test_signup_GET(self):
-
-        response = self.client.get(self.signup_url)
-        self.assertEqual(response.status_code, 200)
-
-    def test_login_GET(self):
-
-        response = self.client.get(self.login_url)
-        self.assertEqual(response.status_code, 200)
-
-    def test_signup_POST(self):
-
-        user_one = User.objects.create_user(
-                                username='newuser', 
-                                email='newuser@email.com',
-                                password='testpass123')
-
-        response = self.client.post(self.signup_url, {'email:', 'newuser@email.com', 'password:', 'testpass123'})
-        self.assertEqual(response.status_code, 200)
-
-
 
 class LoginTests(TestCase):
 
