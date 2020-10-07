1fd20e149ab06aa5e4455f2f35cebae1d48a4e08
Natalie Sterling <nataliemetasterling@gmail.com>
2020-10-07 15:46:32 -0400

    made migrations for userprofile model, updated profile.html, added admin for userprofile

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LandingPageView, IndexPageView
+from .views import LandingPageView, IndexPageView, ProfilePageView
 
 app_name = 'app'
 urlpatterns = [
