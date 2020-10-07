bf513be75123305ad50617bf242939cc25b7e1cf
MichaelKramerGuitar <michael.kramer.guitar@gmail.com>
2020-10-07 12:44:20 -0400

    basic navbar with basic tests to those routes

--- app/tests.py
+++ app/tests.py
@@ -5,6 +5,10 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import View
 from django.http import HttpResponse
 
+from django.test.client import Client
+from django.test.utils import override_settings
+from allauth.utils import get_user_model
+
 from .views import LandingPageView, IndexPageView
 
 
@@ -70,4 +74,59 @@ class IndexPageTests(TestCase):
         request = self.factory.get(reverse('app:index'))
         request.user = self.user
         response = view(request)
+        self.assertEqual(response.status_code, 200)
+class NavBarTests(TestCase):
+    """
+    https://github.com/pennersr/django-allauth/blob/master/allauth/account/tests.py
+    """
+
+
+    def _create_user(self, username="john", password="doe"):
+        user = get_user_model().objects.create(username=username, is_active=True)
+        if password:
+            user.set_password(password)
+        else:
+            user.set_unusable_password()
+        user.save()
+        return user
+
+
+    def _create_user_and_login(self, usable_password=True):
+        password = "doe" if usable_password else False
+        user = self._create_user(password=password)
+        self.client.force_login(user)
+        return user
+
+    def _logout_view(self, method):
+        user = get_user_model().objects.create(username="john", is_active=True)
+        user.set_password("doe")
+        user.save()
+        c = Client()
+        c.login(username="john", password="doe")
+        return c, getattr(c, method)(reverse("account_logout"))
+
+    @override_settings(ACCOUNT_LOGOUT_ON_GET=True)
+    def test_logout_view_on_get(self):
+        c, resp = self._logout_view("get")
+        self.assertTemplateUsed(resp, "account/messages/logged_out.txt")
+
+    def test_login_redirects_to_index_route_when_authenticated(self):
+        self._create_user_and_login()
+        c = self.client
+        response = c.get(reverse("account_login"))
+        self.assertRedirects(response, "/", fetch_redirect_response=False)
+
+    def test_optional_email_verification(self):
+        c = Client()
+        # Signup
+        c.get(reverse("account_signup"))
+        response = c.post(
+            reverse("account_signup"),
+            {
+                "username": "johndoe",
+                "email": "john@example.com",
+                "password1": "johndoe",
+            },
+        )
+        # Logged in
         self.assertEqual(response.status_code, 200)
