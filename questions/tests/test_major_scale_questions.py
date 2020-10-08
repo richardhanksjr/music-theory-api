361dc321d05b837cb038d62e527696a32c812bde
Richard Hanks <richardhanksjr@gmail.com>
2020-10-08 09:41:25 -0400

    Adding back in test_major_scale questions to test Travis build.

--- questions/tests/test_major_scale_questions.py
+++ questions/tests/test_major_scale_questions.py
@@ -1,20 +1,20 @@
-# from django.test import TestCase
-# from questions.questions.major_scale_questions import SimpleScaleDegreeMajor
-# from django.core.cache import cache
-# from questions.models import Question
-#
-#
-# class SimpleScaleDegreeMajorTest(TestCase):
-#     def setUp(self):
-#         Question.objects.create(class_name='SimpleScaleDegreeMajor')
-#         self.question = SimpleScaleDegreeMajor(tonic='C', scale_degree_index=5)
-#
-#     def test_question_added_to_cache(self):
-#         cache_key = self.question.key
-#         question_from_cache = cache.get(cache_key)
-#         self.assertEqual(question_from_cache['answer'], self.question.answer)
-#         self.assertEqual(question_from_cache['question'], self.question.question)
-#         self.assertListEqual(question_from_cache['answer_options'], self.question.answer_options)
-#
-#     def test_correct_weight(self):
-#         self.assertEqual(3, self.question.weight)
+from django.test import TestCase
+from questions.questions.major_scale_questions import SimpleScaleDegreeMajor
+from django.core.cache import cache
+from questions.models import Question
+
+
+class SimpleScaleDegreeMajorTest(TestCase):
+    def setUp(self):
+        Question.objects.create(class_name='SimpleScaleDegreeMajor')
+        self.question = SimpleScaleDegreeMajor(tonic='C', scale_degree_index=5)
+
+    def test_question_added_to_cache(self):
+        cache_key = self.question.key
+        question_from_cache = cache.get(cache_key)
+        self.assertEqual(question_from_cache['answer'], self.question.answer)
+        self.assertEqual(question_from_cache['question'], self.question.question)
+        self.assertListEqual(question_from_cache['answer_options'], self.question.answer_options)
+
+    def test_correct_weight(self):
+        self.assertEqual(3, self.question.weight)
