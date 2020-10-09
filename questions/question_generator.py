cc1931bac52fa25f1863df870bd446dd1b59e968
Richard Hanks <richardhanksjr@gmail.com>
2020-10-09 10:42:18 -0400

    Testing Travis...

--- questions/question_generator.py
+++ questions/question_generator.py
@@ -11,30 +11,30 @@ from .models import Question
 #
 questions = Question.objects.all()
 print("questions*****", questions)
-try:
-    print("questions******************", questions)
-except ProgrammingError as e:
-    print("top block")
-    print("e")
-
-# Dynamically import files
-for question in questions:
-    try:
-        exec(f"from questions.questions.{question.module_name} import {question.class_name}")
-    except ProgrammingError as e:
-        print("error is: ", e)
-
-
-question_choices = questions.values_list('class_name', flat=True)
-
-
-
-class QuestionGenerator:
-
-    @staticmethod
-    def question_factory():
-        """
-        Used to get a random question. Returns a random question instance
-        from question_choices.
-        """
-        return eval(random.choice(question_choices))()
+# try:
+#     print("questions******************", questions)
+# except ProgrammingError as e:
+#     print("top block")
+#     print("e")
+#
+# # Dynamically import files
+# for question in questions:
+#     try:
+#         exec(f"from questions.questions.{question.module_name} import {question.class_name}")
+#     except ProgrammingError as e:
+#         print("error is: ", e)
+#
+#
+# question_choices = questions.values_list('class_name', flat=True)
+#
+#
+#
+# class QuestionGenerator:
+#
+#     @staticmethod
+#     def question_factory():
+#         """
+#         Used to get a random question. Returns a random question instance
+#         from question_choices.
+#         """
+#         return eval(random.choice(question_choices))()
