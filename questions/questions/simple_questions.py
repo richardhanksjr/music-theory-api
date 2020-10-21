a5d0f1451cf7eac68f730acadf1fb4a7f247dab5
Richard Hanks <richardhanksjr@gmail.com>
2020-10-21 15:52:05 -0400

    Added production server to allowed hosts.

--- questions/questions/simple_questions.py
+++ questions/questions/simple_questions.py
@@ -1,3 +1,4 @@
+import random
 from .questions import Question
 
 
@@ -24,3 +25,76 @@ class SimpleIntervalIs(Question):
 
     def _generate_question_params(self):
         self._question_params = {'question_type': self.question_type}
+
+
+class InvertedQualityIs(Question):
+
+    def __init__(self, interval_quality=None, same=None):
+        self.interval_quality = interval_quality if interval_quality else random.choice(['major', 'minor', 'diminished', 'augmented', 'perfect'])
+        self.same = same if same is not None else random.choice([True, False])
+        self.formatted_remains = "remains" if self.same is True else "does NOT remain"
+        super().__init__()
+
+    def _generate_question(self):
+        self._question = f"If a {self.interval_quality.lower()} interval is inverted it {self.formatted_remains} {self.interval_quality}"
+
+    def _generate_answer(self):
+        if self.interval_quality.lower() == 'perfect':
+            self._answer = "True" if self.same is True else "False"
+        else:
+            self._answer = "False" if self.same is True else "True"
+
+    def _generate_answer_options(self):
+        self._answer_options = ["True", "False"]
+
+    def _generate_help_steps_array(self):
+        self._help_steps = ({'prompt': 'How are interval qualities affected by inversion?',
+                           'answer': 'Perfect -> Perfect, Major -> minor, minor -> Major, diminished '
+                                     '-> Augmented, Augmented -> diminished'}, )
+
+    def _generate_question_type(self):
+        self._question_type = 'inverted_quality_is'
+
+    def _generate_question_params(self):
+        self._question_params = {}
+
+
+class TritoneIs(Question):
+    def _generate_question(self):
+        self._question = "A TRITONE is:"
+
+    def _generate_answer(self):
+        self._answer = "An augmented 4th"
+
+    def _generate_answer_options(self):
+        self._answer_options = ["An augmented 4th", "a three note melody", "a three tone chord", "a diminished 4th"]
+
+    def _generate_help_steps_array(self):
+        self._help_steps = ({"prompt": "What is a tritone?", "answer": "an interval of three whole tones (an augmented fourth), "
+                                                                       "as between C and F sharp."},)
+
+    def _generate_question_type(self):
+        self._question_type = 'tritone-is'
+
+    def _generate_question_params(self):
+        self._question_params = {}
+
+
+class CouldBePerfectInterval(Question):
+    def _generate_question(self):
+        self._question = "All of the following could be perfect intervals except:"
+
+    def _generate_answer(self):
+        self._answer = "Third"
+
+    def _generate_answer_options(self):
+        self._answer_options = ["Fourth", "Third", "Prime", "Octave"]
+
+    def _generate_help_steps_array(self):
+        self._help_steps = ({"prompt": "What is a perfect interval?", "answer": "an interval who's inversion is also perfect"},)
+
+    def _generate_question_type(self):
+        self._question_type = 'could_be_perfect_interval'
+
+    def _generate_question_params(self):
+        self._question_params = {}
--- questions/tests/test_simple_questions.py
+++ questions/tests/test_simple_questions.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from questions.questions.simple_questions import SimpleIntervalIs
+from questions.questions.simple_questions import SimpleIntervalIs, InvertedQualityIs, TritoneIs, CouldBePerfectInterval
 from django.core.cache import cache
 from questions.models import Question, Tag
 
@@ -29,3 +29,74 @@ class SimpleIntervalIsTest(TestCase):
 
     def test_correct_weight(self):
         self.assertEqual(1, self.question.weight)
+
+
+class InvertedQualityIsTest(TestCase):
+
+    def test_same_perfect_true(self):
+        question = InvertedQualityIs(interval_quality='perfect', same=True)
+        self.assertEquals("True", question.answer)
+
+    def test_not_same_perfect_false(self):
+        question = InvertedQualityIs(interval_quality='perfect', same=False)
+        self.assertEquals("False", question.answer)
+
+    def test_same_major_false(self):
+        question = InvertedQualityIs(interval_quality='major', same=True)
+        self.assertEquals("False", question.answer)
+
+    def test_not_same_major_true(self):
+        question = InvertedQualityIs(interval_quality='major', same=False)
+        self.assertEquals("True", question.answer)
+
+    def test_same_minor_false(self):
+        question = InvertedQualityIs(interval_quality='minor', same=True)
+        self.assertEquals("False", question.answer)
+
+    def test_not_same_minor_true(self):
+        question = InvertedQualityIs(interval_quality='minor', same=False)
+        self.assertEquals("True", question.answer)
+
+    def test_same_diminished_false(self):
+        question = InvertedQualityIs(interval_quality='diminished', same=True)
+        self.assertEquals("False", question.answer)
+
+    def test_not_same_diminished_true(self):
+        question = InvertedQualityIs(interval_quality='diminished', same=False)
+        self.assertEquals("True", question.answer)
+
+    def test_same_augmented_false(self):
+        question = InvertedQualityIs(interval_quality='augmented', same=True)
+        self.assertEquals("False", question.answer)
+
+    def test_not_same_augmented_true(self):
+        question = InvertedQualityIs(interval_quality='augmented', same=False)
+        self.assertEquals("True", question.answer)
+
+
+class TritoneIsTest(TestCase):
+
+    def setUp(self):
+        self.question = TritoneIs()
+
+    def test_question(self):
+        expected_question = "A TRITONE is:"
+        self.assertEqual(expected_question, self.question.question)
+
+    def test_answer(self):
+        expected_answer = "An augmented 4th"
+        self.assertEqual(expected_answer, self.question.answer)
+
+
+class CouldBePerfectIntervalTest(TestCase):
+
+    def setUp(self):
+        self.question = CouldBePerfectInterval()
+
+    def test_question(self):
+        expected_question = "All of the following could be perfect intervals except:"
+        self.assertEqual(expected_question, self.question.question)
+
+    def test_answer(self):
+        expected_answer = "Third"
+        self.assertEqual(expected_answer, self.question.answer)
