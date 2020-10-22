983c5808e17c0e8251869f5433e80fe17f1b1d38
Natalie Sterling <nataliemetasterling@gmail.com>
2020-10-22 10:20:07 -0400

    removed str from settings

--- music_theory_api/settings.py
+++ music_theory_api/settings.py
@@ -73,7 +73,7 @@ ROOT_URLCONF = 'music_theory_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [str(BASE_DIR.joinpath('templates'))],
+        'DIRS': [BASE_DIR.joinpath('templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
