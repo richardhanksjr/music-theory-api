3efc7b2075d713f2519ea1264e79d7385f92736c
Richard Hanks <richardhanksjr@gmail.com>
2020-10-28 10:34:32 -0400

    Added STATICFILES_DIRS to settings.py.

--- music_theory_api/settings.py
+++ music_theory_api/settings.py
@@ -154,6 +154,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 # django-crispy-forms
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
