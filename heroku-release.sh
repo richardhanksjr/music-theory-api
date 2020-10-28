cfc752de1bee479f62a35995a628ba149a1c8410
Richard Hanks <richardhanksjr@gmail.com>
2020-10-28 09:44:46 -0400

    Moved collectstatic to the end of the heroku-release.sh script.

--- heroku-release.sh
+++ heroku-release.sh
@@ -2,6 +2,6 @@
 
 python manage.py migrate
 python manage.py createcachetable
-python manage.py collectstatic --noinput
 python manage.py loaddata questions.json
 npm run dev
+python manage.py collectstatic --noinput
