477f7c913c5c8d63fedfb7c6a94133fb804ded08
Richard Hanks <richardhanksjr@gmail.com>
2020-10-28 10:43:14 -0400

    Removed STATICFILES_DIR.

--- music_theory_api/settings.py
+++ music_theory_api/settings.py
@@ -154,9 +154,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # django-crispy-forms
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
