10059227260545f643fbb588e74729029742b42e
nkawaller <63615939+nkawaller@users.noreply.github.com>
2020-11-08 12:47:10 -0500

    added attempt admin with list display

--- questions/admin.py
+++ questions/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Question, Tag, Attempt
 
+@admin.register(Attempt)
+class AttemptAdmin(admin.ModelAdmin):
+    list_display = ("user", "question", "correct")
+
 admin.site.register(Question)
 admin.site.register(Tag)
-admin.site.register(Attempt)
