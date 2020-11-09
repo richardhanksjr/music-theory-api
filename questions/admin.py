c0e40cb11258a6b68660273b59c225b986fa0554
nkawaller <63615939+nkawaller@users.noreply.github.com>
2020-11-09 07:01:30 -0500

    added list filter

--- questions/admin.py
+++ questions/admin.py
@@ -4,6 +4,7 @@ from .models import Question, Tag, Attempt
 @admin.register(Attempt)
 class AttemptAdmin(admin.ModelAdmin):
     list_display = ("user", "question", "correct")
+    list_filter = ('question',)
 
 
 admin.site.register(Question)
