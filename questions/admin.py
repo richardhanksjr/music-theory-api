08e40c1356eace8681b3bc6267680c1982263fce
nkawaller <63615939+nkawaller@users.noreply.github.com>
2020-11-09 15:43:31 -0500

    average

--- questions/admin.py
+++ questions/admin.py
@@ -4,10 +4,15 @@ from .models import Question, Tag, Attempt
 @admin.register(Attempt)
 class AttemptAdmin(admin.ModelAdmin):
     list_display = ("user", "question", "correct", "number_correct", 
-                    "number_incorrect", "total_attempts")
+                    "number_incorrect", "total_attempts", "percent_correct")
     list_filter = ("user", "question", "correct", "number_correct", 
                     "number_incorrect", "total_attempts")
     search_fields = ('user__username',)
+
+    def percent_correct(self, obj):
+        from django.db.models import Avg
+        result = Attempt.objects.filter(user=obj.user).aggregate(Avg('number_correct'))
+        return result['number_correct__avg']
 
 
 admin.site.register(Question)
