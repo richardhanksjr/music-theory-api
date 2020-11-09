f168367b825ea408efdd98acfeba11964d3f6e61
nkawaller <63615939+nkawaller@users.noreply.github.com>
2020-11-09 09:12:30 -0500

    added model fields (correct/incorrect/total)

--- questions/admin.py
+++ questions/admin.py
@@ -4,7 +4,8 @@ from .models import Question, Tag, Attempt
 @admin.register(Attempt)
 class AttemptAdmin(admin.ModelAdmin):
     list_display = ("user", "question", "correct")
-    list_filter = ('question',)
+    list_filter = ('user', 'question', 'correct')
+    search_fields = ('user__username',)
 
 
 admin.site.register(Question)
